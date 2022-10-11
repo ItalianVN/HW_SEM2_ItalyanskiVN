7fa933d1b1460111759612f4dba8fc06b126bea4
Vyacheslav <elik@example.com>
2022-10-11 13:30:43 +0700

    add task#10

---
+++ zad1/Program.cs
@@ -0,0 +1,16 @@
+﻿void secondnumber()
+{
+Console.WriteLine("ВВедите трехзначное число: ");    
+int newNumeric = int.Parse(Console.ReadLine());
+if (newNumeric>99 && newNumeric<1000)
+{
+    string numericAsString = newNumeric.ToString();
+    int result = int.Parse(numericAsString[1].ToString());
+    Console.WriteLine($"Result numeric:{result}");
+}
+else 
+Console.WriteLine("Число не трехзначное "); 
+}
+
+secondnumber();
+
