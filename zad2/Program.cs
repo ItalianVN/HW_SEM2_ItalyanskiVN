e5192eeb5b2f5901bda5f70f3a56e1bfa0bff33c
Vyacheslav <elik@example.com>
2022-10-11 13:32:05 +0700

    add task#13

---
+++ zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿void secondnumber()
+{
+Console.WriteLine("ВВедите число: ");    
+int newNumeric = int.Parse(Console.ReadLine());
+if (newNumeric>99)
+{
+    string numericAsString = newNumeric.ToString();
+    int result = int.Parse(numericAsString[2].ToString());
+    Console.WriteLine($"Result numeric:{result}");
+}
+else 
+Console.WriteLine("Треттьей цифры нет"); 
+}
+
+secondnumber();
+
+
